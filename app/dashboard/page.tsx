8b9917a47933a4faad61c1a0c3b41c2e540058dc
ITZSHOAIB <skshoaibpersonal@gmail.com>
2024-01-07 23:17:53 +0530

    localhost connection implemented

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+import { useReadContract } from "wagmi";
+import artifact from "@/artifacts/VowedOnChain.json";
+import contractAddress from "@/artifacts/contract-address.json";
+const { abi } = artifact;
+const { VowedOnChainAddress } = contractAddress;
+
 export default function Dashboard() {
+  const {
+    data: data,
+    error: error,
+    isSuccess: success,
+  } = useReadContract({
+    abi: abi,
+    address: VowedOnChainAddress as `0x${string}`,
+    functionName: "getMaritalStatus",
+  });
+  console.log("contract gift balance ", data, error, success);
   return <></>;
 }
--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,18 +1,19 @@
 import { http, createConfig, cookieStorage, createStorage } from "wagmi";
-import { base, mainnet } from "wagmi/chains";
+import { base, localhost, mainnet } from "wagmi/chains";
 import { injected, metaMask, safe, walletConnect } from "wagmi/connectors";
 
 const projectId = `${process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID}`;
 
 export const wagmiConfig = createConfig({
-  chains: [mainnet, base],
+  chains: [localhost],
   ssr: true,
   storage: createStorage({
     storage: cookieStorage,
   }),
   connectors: [injected(), walletConnect({ projectId }), metaMask(), safe()],
   transports: {
-    [mainnet.id]: http(),
-    [base.id]: http(),
+    [localhost.id]: http(),
+    // [mainnet.id]: http(),
+    // [base.id]: http(),
   },
 });
